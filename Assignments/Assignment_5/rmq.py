f49f1cbd4e97485f6505b595560b543daf2cd2b4
Josh <cullingsjosh@gmail.com>
2019-11-28 11:12:54 -0700

    Still needs work

---
+++ Assignments/Assignment_5/rmq.py
@@ -0,0 +1,45 @@
+#########################
+# Author: Josh Cullings #
+#                       #
+# Date: 11/28/2019      #
+#########################
+
+import sys
+
+def rmq(table, lower, upper):
+    print("Entered into rmq with lower: %d and upper: %d" %(lower,upper))
+
+def sparse_table(sequences):
+    seq_out = [[0 for x in range(len(sequences))] for y in range(len(sequences))]
+    seq_out[0] = sequences
+    for j in range(sequences.length):
+        for i in range(sequences.length):
+            if((i+j)<(sequences.length)):
+                seq_out[j][i+j] = min(sequences[i:i+j])
+            else:
+                break
+    return seq_out
+
+# From assignment 1
+# Added fasta file
+if len(sys.argv) > 1:  # if command line input exists perform neccessary actions
+    fasta_file = open(sys.argv[1], 'r')
+
+    sequences = fasta_file.split(",")
+    print(sequences)
+    table = sparse_table(sequences)
+    while(True):
+        try:
+            cont = input("Continue? (y/n) ")
+            if(cont == 'y'):
+                lower = int(input("Please type the lower bound for sequence size %d: " %len(sequences)))
+                upper = int(input("Please type the upper bound for sequence size %d, greater than lower bound %d: " %(len(sequences),lower)))
+                rmq(table, lower, upper)
+            elif(cont == 'n'):
+                break
+            else:
+                print("Incorrect input")
+        except:
+            print("Incorrect input")
+else:
+    quit()
